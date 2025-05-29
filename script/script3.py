ebcc9a25362f707b944678617cb435a980c356be
Ramon <ramonjscorrea@gmail.com>
2025-05-29 17:56:12 -0300

    Inverção da ordem de script no main, e atualização na renomeação dos arquivos

--- script/script3.py
+++ script/script3.py
@@ -1,19 +1,26 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 downloads = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-def pegar_data_atual():
-    return datetime.now()
+def pegar_data_anterior():
+    ontem = datetime.now() - timedelta(days=1)
+    return ontem.strftime('%d_%m_%Y')
 
-# Formato da data no arquivo: '29_05_2025'
-data = pegar_data_atual().strftime('%d_%m_%Y')
+def pegar_data_atual():
+    return datetime.now().strftime('%d_%m_%Y')
+
+# Formato data de ontem
+data_ontem = pegar_data_anterior()
+
+# Formato da data de hoje
+data = pegar_data_atual()
 
 palavra_chave1 = 'historico_de_chamados'
-novo_nome1 = 'historico_final.csv'
+novo_nome1 = f'DB_HISTÓRICO-CHAMADOS_{data_ontem}.csv'
 
 palavra_chave2 = 'estatisticas-avaliacoes'
-novo_nome2 = 'historico_final2.csv'
+novo_nome2 = f"DB_NPS_{data_ontem}.csv"
 
 def encontrar_e_Renomear(data, palavra_chave, novo_nome):
     encontrado = False
