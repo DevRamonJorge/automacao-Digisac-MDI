5d95586970cf80b5d2fc1c8532336006381797a0
Ramon <ramonjscorrea@gmail.com>
2025-05-30 17:45:47 -0300

    Adicionando ao Main

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import subprocess
 subprocess.run(["python", "script/script2.py"])
 subprocess.run(["python", "script/script.py"])
 subprocess.run(["python", "script/script3.py"])
+subprocess.run(["python", "script/script4.py"])
