d393901be81340d6c33e733467907e6c5271cf0d
Ramon <ramonjscorrea@gmail.com>
2025-06-03 13:34:57 -0300

    Correção bug script 3

--- script/script3.py
+++ script/script3.py
@@ -3,22 +3,22 @@ from datetime import datetime, timedelta
 
 downloads = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-# Data usada para encontrar o arquivo: sempre ontem
-def pegar_data_ontem():
-    ontem = datetime.now() - timedelta(days=1)
-    return ontem.strftime('%d_%m_%Y')
+# Data de hoje para encontrar os arquivos
+def pegar_data_hoje():
+    hoje = datetime.now()
+    return hoje.strftime('%d_%m_%Y')
 
-# Data usada para o novo nome: sexta-feira se hoje for segunda, senão ontem
+# Data usada para o novo nome: sexta-feira se hoje for segunda, senão hoje
 def pegar_data_para_nome():
     hoje = datetime.now()
     if hoje.weekday() == 0:  # Segunda-feira
         sexta = hoje - timedelta(days=3)
         return sexta.strftime('%d_%m_%Y')
     else:
-        return pegar_data_ontem()
+        return hoje.strftime('%d_%m_%Y')
 
 # Datas
-data_ontem = pegar_data_ontem()
+data_hoje = pegar_data_hoje()
 data_para_nome = pegar_data_para_nome()
 
 # Nomes novos com base na data ajustada
@@ -28,7 +28,7 @@ novo_nome1 = f'DB_HISTÓRICO-CHAMADOS_{data_para_nome}.csv'
 palavra_chave2 = 'estatisticas-avaliacoes'
 novo_nome2 = f"DB_NPS_{data_para_nome}.csv"
 
-def encontrar_e_Renomear(data, palavra_chave, novo_nome):
+def encontrar_e_renomear(data, palavra_chave, novo_nome):
     encontrado = False
     for arquivo in os.listdir(downloads):
         if data in arquivo and palavra_chave in arquivo:
@@ -41,6 +41,6 @@ def encontrar_e_Renomear(data, palavra_chave, novo_nome):
     if not encontrado:
         print("❌ Nenhum arquivo com a data e nome informados foi encontrado na pasta Downloads.")
 
-# Buscar arquivos com data de ontem, renomear com a data correta
-encontrar_e_Renomear(data_ontem, palavra_chave1, novo_nome1)
-encontrar_e_Renomear(data_ontem, palavra_chave2, novo_nome2)
+# Buscar arquivos com data de hoje, renomear com a data correta
+encontrar_e_renomear(data_hoje, palavra_chave1, novo_nome1)
+encontrar_e_renomear(data_hoje, palavra_chave2, novo_nome2)
